e2e32a98b6d4d41df8da3e7e91abf7d77d44e399
nmartins0611 <nmartins@redhat.com>
2024-01-04 12:37:24 +0200

    Create callback.bat

---
+++ callback.bat
@@ -0,0 +1,4 @@
+@echo off
+title Compliance trigger
+echo Compliance change
+## EXAMPLE curl -k -s --data "host_config_key=0102030405windows" https://controller.ansible-automates.co.za/api/v2/job_templates/29/callback/ 
